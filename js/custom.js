7f5872f6ec5afe53969f50243c6c7e94c757e926
kido-fullstack <ziauddin.sayyed@kido.school>
2022-07-27 17:57:23 +0530

    Added contact id

--- js/custom.js
+++ js/custom.js
@@ -90,7 +90,7 @@ var access_portals_list = [
     },
     {
         "name": "Kido Famly App",
-        "link": "https://www.famly.co/",
+        "link": "https://app.famly.co/#/login",
         "logo": "https://storage.googleapis.com/kido-assets/famly_logo.png",
         "desc": "Software that brings you closer to parents and makes it easier to run your setting smoothly.",
     },
